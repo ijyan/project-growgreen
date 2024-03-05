713d24b9b63fced347bfda7e7e3b939d381fac21
ijyan <guuhwi@gmail.com>
2024-03-06 05:04:44 +0900

    #15 Add: link 추가

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import * as S from './index.Style';
 import Img from '../../assets/images/not-found.jpg';
 
 // 404 에러 페이지
 export default function Index() {
+  const navigate = useNavigate();
   return (
     <S.Content>
       <img src={Img} alt="페이지를 찾을 수 없습니다." />
@@ -15,10 +17,18 @@ export default function Index() {
           요청하신 페이지의 주소가 변경, 삭제되어 페이지를 찾을 수 없습니다.
         </p>
         <div>
-          <button type="button" className="btn_home">
+          <button
+            type="button"
+            className="btn_home"
+            onClick={() => navigate('/')}
+          >
             홈으로 가기
           </button>
-          <button type="button" className="btn_prev">
+          <button
+            type="button"
+            className="btn_prev"
+            onClick={() => navigate(-1)}
+          >
             이전 페이지
           </button>
         </div>
